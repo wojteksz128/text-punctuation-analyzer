9e1a9e9d194dfb43e704d4d08b77a2b493ad925a
Wojciech Szczepaniak <szczepaniak.brachowice@gmail.com>
2020-07-20 10:04:13 +0200

    Showing information about word classification

--- tpa-polish-lang/src/main/java/net/wojteksz128/tpa/polish/main.kt
+++ tpa-polish-lang/src/main/java/net/wojteksz128/tpa/polish/main.kt
@@ -1,6 +1,8 @@
 package net.wojteksz128.tpa.polish
 
 import net.wojteksz128.tpa.TextPunctuationAnalyzer
+import net.wojteksz128.tpa.utils.morfeusz.MorfeuszTagDecode
+import pl.sgjp.morfeusz.Morfeusz
 
 /*
 * Examples:
@@ -12,6 +14,7 @@ import net.wojteksz128.tpa.TextPunctuationAnalyzer
 
 fun main(args: Array<String>) {
     val analyzer: TextPunctuationAnalyzer = PolishTextPunctuationAnalyzerImpl()
+    val instance: Morfeusz = Morfeusz.createInstance()
 
     args.forEach { text ->
         println("----------")
@@ -23,6 +26,15 @@ fun main(args: Array<String>) {
         val result = analyzer.analyze(text)
         1.rangeTo(recognizingText.length).forEach { _ -> print("\b \b") }
         println("\rRozpoznano:")
-        result.textParts.forEach { println(" -\t${it.javaClass.simpleName} '${it.get()}' na pozycji ${it.startAt}-${it.endAt}") }
+        result.textParts.forEach {
+            println(" -\t${it.javaClass.simpleName} '${it.get()}' na pozycji ${it.startAt}-${it.endAt}")
+            val analyzed = instance.analyseAsList(it.get())
+            analyzed.forEach { morthInterpretation ->
+                println("\t -\t${morthInterpretation.orth}, ${morthInterpretation.lemma}, ${morthInterpretation.getTag(instance)}")
+                val decoded = MorfeuszTagDecode.decode(morthInterpretation.getTag(instance))
+                println("\t\t$decoded")
+            }
+            println()
+        }
     }
 }
