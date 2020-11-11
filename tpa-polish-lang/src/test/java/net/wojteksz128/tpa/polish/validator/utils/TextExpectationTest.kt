a553246ebbca509eea42e5a23a558356384dd19f
Wojciech Szczepaniak <szczepaniak.brachowice@gmail.com>
2020-11-11 15:32:07 +0100

    Change test name format

--- tpa-polish-lang/src/test/java/net/wojteksz128/tpa/polish/validator/utils/TextExpectationTest.kt
+++ tpa-polish-lang/src/test/java/net/wojteksz128/tpa/polish/validator/utils/TextExpectationTest.kt
@@ -1,3 +1,7 @@
 package net.wojteksz128.tpa.polish.validator.utils
 
-data class TextExpectationTest(val text: String, val expected: List<Group>)
+data class TextExpectationTest(val text: String, val expected: List<Group>) {
+    override fun toString(): String {
+        return "text = '$text', expected = List<Group>(size = ${expected.size}, items = $expected)"
+    }
+}
